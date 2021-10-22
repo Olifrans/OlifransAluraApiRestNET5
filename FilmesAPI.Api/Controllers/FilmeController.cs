13b631c57eb67a9ab3c0a62e558cce3d64a87683
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2021-10-22 08:58:17 -0300

    Controller FilmeAPI

---
+++ FilmesAPI.Api/Controllers/FilmeController.cs
@@ -0,0 +1,31 @@
+﻿using FilmesAPI.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+
+        [HttpPost] //Criar recurso no sistema
+        public void AdcionarFilme([FromBody]Filme filme)
+        {
+            ////Validação simples e manual dos parâmetros recebidos, ineficiente --> sobrecarregar a controller
+            //if (!string.IsNullOrEmpty(filme.Titulo))
+            //    filmes.Add(filme);
+            //else
+            //    Console.WriteLine("Não pode ter titulo em branco");
+
+            filmes.Add(filme);
+            //Console.WriteLine(filme.Titulo);
+        }
+
+
+
+    }
+}
---
+++ FilmesAPI.Api/Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Api.Models
+{
+    public class Filme
+    {
+        //public int Id { get; set; }
+        [Required] ////Validação eficiente sem sobrecarregar a controller
+        public string Titulo { get; set; }
+
+        [Required]
+        public string Diretor { get; set; }
+
+        [Required]
+        public string Genero { get; set; }
+
+        [Required]
+        [Range(1, 5600)] ////Validação eficiente sem sobrecarregar tempo de exibição do filme
+        public string Duracao { get; set; }
+    }
+}
